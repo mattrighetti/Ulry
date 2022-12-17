009d597521586063983f826b4358569ed30d3cc5
Mattia Righetti <matt95.righetti@gmail.com>
2022-12-17 17:00:56 +0100

    Appearence updates
    + New iPhone 14 Pro mockups
    + Curated links
    + New shortcut feature

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,11 +15,12 @@ const Home: NextPage = () => {
             </Head>
             <Hero />
             <ButtonSection />
-            <Feature img="/images/home-image.png" title="Minimalistic UI" body="Ulry was designed with simplicity in mind, the user interface is crafted to show you what really matters." odd={true} />
-            <Feature img="/images/list-image.png" title="All in a single place" body="Find all your favourite links in a single place. You can order your links, you can star them and you can also check which you've read and which you haven't and much more..." odd={false} />
-            <Feature img="/images/detail-image.png" title="Need more details?" body="Ulry can show you more info about a particular link, you can even add notes to every single one so that you won't forget what you wanted to do with it." odd={true} />
-            <Feature img="/images/add-group-image.png" title="Highly customisable" body="Everything that you see in Ulry is under your control, you can change group icons and colors, same goes for tags." odd={false} />
-            <Feature img="/images/url-redirector-image.png" title="Redirections" body="Are you tired of paywalled articles or websites that require you to sign in? Ulry got you covered! With redirections you can tell Ulry which websites you would like to be redirected to for specific websites" odd={true} />
+            <Feature img="/images/home.png" title="Minimalistic UI" body="Ulry was designed with simplicity in mind, the user interface is crafted to show you what really matters." odd={true} />
+            <Feature img="/images/link-list.png" title="All in a single place" body="Find all your favourite links in a single place. You can order them, you can star them and you can also check which you've read and which you haven't and much more..." odd={false} />
+            <Feature img="/images/details-2.png" title="Need more details?" body="Ulry can show you more info about a particular link, you can even attach a note to it so that you won't forget what you wanted to do with it." odd={true} />
+            <Feature img="/images/add-shortcut.png" title="Save from everywhere" body="Ulry comes with useful shortcuts that will let you save your favourite articles and websites directly from Safari and other apps on your phone." odd={false} />
+            <Feature img="/images/add-group.png" title="Highly customisable" body="Everything that you see in Ulry is under your control, you can change group icons and colors, same goes for tags." odd={false} />
+            <Feature img="/images/redirector.png" title="Redirections" body="Are you tired of paywalled articles or websites that require you to sign in? Ulry got you covered! With redirections you can tell Ulry which websites you would like to be redirected to for specific websites" odd={true} />
             <Footer />
         </>
     )
